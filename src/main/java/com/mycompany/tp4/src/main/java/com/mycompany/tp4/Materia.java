6f167ed4b079a8ad06b48ce0ec6930b5b3eb8d61
cynthiaurquizag <urquizacma23@gmail.com>
2026-09-18 15:40:46 -0300

    gui
    falta conectar eventos y los hashset

---
+++ src/main/java/com/mycompany/tp4/src/main/java/com/mycompany/tp4/Materia.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.tp4;
+
+public class Materia {
+    private int idMateria;
+   private String nombre;
+    private int anio;
+   
+    Materia (int idMateria, String nombre, int anio){
+        this.anio = anio;
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+    }
+    
+    public int getIdMateria (){
+        return idMateria;
+    }
+    
+    public void setIdMateria(int idMateria){
+        this.idMateria = idMateria;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public int getAnio(){
+        return anio;
+    }
+    
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+    
+    
+}
